128a0c21f88b7a1811e03e59cfd61a5be1aa9204
Ekaterina Kozyreva <83861300+E-Kozyreva@users.noreply.github.com>
2022-03-09 20:19:18 +0300

    Больше среднего
    Напишите программу, которая выводит количество элементов квадратной матрицы в каждой строке, больших среднего арифметического элементов данной строки.
    
    Формат входных данных:
    На вход программе подаётся натуральное число n — количество строк и столбцов в матрице, затем элементы матрицы (целые числа) построчно через пробел.
    
    Формат выходных данных:
    Программа должна вывести n чисел — для каждой строки количество элементов матрицы, больших среднего арифметического элементов данной строки.

---
+++ PYTHON/Stepik/4_4_4.py
@@ -0,0 +1,12 @@
+def matrix_string(count):
+    for _ in range(count):
+        flag = 0
+        matrix = list(map(int, input().split()))
+        for num in range(count):
+            if sum(matrix) / count < matrix[num]:
+                flag += 1
+        print(flag)
+        matrix.clear()
+
+                      
+matrix_string(int(input()))
