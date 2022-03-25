39d8ecb188ab839b648874a75f9a5c404292decb
Ekaterina Kozyreva <83861300+E-Kozyreva@users.noreply.github.com>
2022-03-25 15:43:37 +0300

    Конкурсный отбор
    Напишите программу, которая выводит список хорошистов и отличников в классе.
    
    Формат входных данных:
    На вход программе подается натуральное число nn, далее следует nn строк с фамилией школьника и его оценкой на каждой из них.
    
    Формат выходных данных:
    Программа должна вывести сначала все введённые строки с фамилиями и оценками учеников в том же порядке. Затем следует пустая строка, а затем выводятся строки с фамилиями и оценками хорошистов и отличников (в том же порядке).
    
    Примечание 1. Оценка ученика – это натуральное число от 11 до 55.
    Примечание 2. Гарантируется, что в классе есть хотя бы один хорошист – обладатель оценки 44, или отличник – получивший 55.

---
+++ PYTHON/Stepik/6_3_6.py
@@ -0,0 +1,25 @@
+def ExcellentAndGood(students):
+    StudentsList_EG = []
+    
+    for student in range(len(students)):
+        mark = int(students[student][-1])
+        if mark == 4 or mark == 5:
+            StudentsList_EG.append(students[student])
+            print(students[student])
+            
+    return StudentsList_EG
+
+
+def Students(count):
+    StudentsList = []
+    
+    for student in range(count):
+        StudentsList.append(input())
+        print(StudentsList[student])
+        
+    return StudentsList
+
+
+StudentsList = Students(int(input()))
+print()
+ExcellentAndGood(StudentsList)
