0868b174be6158b7b6f35bb898512764a8de31ea
Ekaterina Kozyreva <83861300+E-Kozyreva@users.noreply.github.com>
2022-03-25 15:45:39 +0300

    Вершина параболы
    Уравнение параболы имеет вид y =ax^2 + bx + c, где a != 0. Напишите программу, которая по введенным значениям a, b, c определяет и выводит вершину параболы.
    
    Формат входных данных:
    На вход программе подаются три целых числа, каждое на отдельной строке.
    
    Формат выходных данных:
    Программа должна вывести координаты вершины параболы.

---
+++ PYTHON/Stepik/6_3_5.py
@@ -0,0 +1,8 @@
+def TopOfParabola(a, b, c):
+    x = -(b / (2*a))
+    y = ((4 * a * c) - (b ** 2)) / (4 * a)
+    
+    return x, y
+
+a, b, c = int(input()), int(input()), int(input())
+print(TopOfParabola(a, b, c))
