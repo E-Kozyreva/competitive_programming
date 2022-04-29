f3eaae8a71c4564220b00623a82ede71d5c7bc22
Ekaterina Kozyreva <83861300+E-Kozyreva@users.noreply.github.com>
2022-04-29 15:59:38 +0300

    Условный оператор
    Даны два целых числа. Выведите значение наибольшего из них. Если числа равны, выведите любое из них.
    
    Формат входных данных:
    Вводятся два числа.
    
    Формат выходных данных:
    Выведите ответ на задачу.

---
+++ C++/STEPIK/Введение в программирование (C++)/1_4_1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+void MaxNumber(int num1, int num2) 
+{
+    if (num1 > num2){
+        std::cout << num1;
+    } else if (num1 < num2) {
+        std::cout << num2;
+    } else {
+        std::cout << num1;
+    }
+}
+
+int main() 
+{
+    int num1, num2;
+    std::cin >> num1;
+    std::cin >> num2;
+    
+    MaxNumber(num1, num2);
+    return 0;
+}
