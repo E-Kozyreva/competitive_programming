50c9ca64285b583d080acf495392587fd3673d9c
Ekaterina Kozyreva <83861300+E-Kozyreva@users.noreply.github.com>
2022-04-29 15:58:14 +0300

    Условный оператор
    Даны три целых числа. Определите, сколько среди них совпадающих. Программа должна вывести одно из чисел: 3 (если все совпадают), 2 (если два совпадает) или 0 (если все числа различны).
    
    Формат входных данных:
    Вводятся три числа.
    
    Формат выходных данных:
    Выведите ответ на задачу.

---
+++ C++/STEPIK/Введение в программирование (C++)/1_4_5.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int FindRepeat(int n1, int n2, int n3)
+{
+    int count;
+    if (n1 == n2 && n2 == n3) {
+        count = 3;
+    } else if (n1 == n2 && n1 != n3 && n2 != n3) {
+        count = 2;
+    } else if (n1 != n2 && n1 == n3 && n2 != n3) {
+        count = 2;
+    } else if (n1 != n2 && n1 != n3 && n2 == n3) {
+        count = 2;
+    } else {
+        count = 0;
+    }
+    return count;
+}
+
+int main() 
+{
+    int num1, num2, num3;
+    std::cin >> num1 >> num2 >> num3;
+    
+    std::cout << FindRepeat(num1, num2, num3);
+    return 0;
+}
