12e8950b97eb5aa8ea754ffcad4bbddca6f09466
Ekaterina Kozyreva <83861300+E-Kozyreva@users.noreply.github.com>
2022-04-29 16:00:40 +0300

    Условный оператор
    Даны два целых числа. Программа должна вывести единицу, если первое число больше второго, двойку, если второе больше первого, или ноль, если они равны.
    
    Формат входных данных:
    Вводятся два числа.
    
    Формат выходных данных:
    Выведите ответ на задачу.

---
+++ C++/STEPIK/Введение в программирование (C++)/1_4_2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int MaxNumber(int num1, int num2) 
+{
+    if (num1 > num2){
+        return 1;
+    } else if (num1 < num2) {
+        return 2;
+    }
+    return 0;
+}
+
+int main() 
+{
+    int num1, num2;
+    std::cin >> num1 >> num2;
+    
+    std::cout << MaxNumber(num1, num2);
+    return 0;
+}
