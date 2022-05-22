dbb1792701602a3359fdf7c0b7797ea2ab6388e6
Ekaterina Kozyreva <83861300+E-Kozyreva@users.noreply.github.com>
2022-05-22 12:34:55 +0300

    сумма цифр трехзначного числа
    Дано целое трехзначное число. Найдите сумму его цифр.
    
    Формат входных данных
    На вход дается число от 100 до 999.
    
    Формат выходных данных
    Выведите одно целое число - ответ на задачу.

--- C++/STEPIK/Введение в программирование (C++)/1_3_3.cpp
+++ C++/STEPIK/Введение в программирование (C++)/1_3_3.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-#include <string>     
-using namespace std;  
 
 int main()
 {
     int number;
-    cin >> number;
-    cout << number % 10;
+    std::cin >> number;
+    std::cout << number % 10;
     return 0;
 }
