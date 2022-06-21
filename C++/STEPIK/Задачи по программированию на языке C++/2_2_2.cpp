1ff327e8d07c906a33a8c04d9b534beeed5c659f
Ekaterina Kozyreva <83861300+E-Kozyreva@users.noreply.github.com>
2022-06-21 13:54:05 +0300

    обратный порядок в строку
    Напишите программу, которая запрашивает четыре целых числа и выводит их в обратном порядке по одному в строке.

---
+++ C++/STEPIK/Задачи по программированию на языке C++/2_2_2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() 
+{
+  
+  int num1, num2, num3, num4;
+  std::cin >> num1 >> num2 >> num3 >> num4;
+    
+  std::cout << num4 << std::endl;
+  std::cout << num3 << std::endl;
+  std::cout << num2 << std::endl;
+  std::cout << num1 << std::endl;
+    
+  return 0;
+}
