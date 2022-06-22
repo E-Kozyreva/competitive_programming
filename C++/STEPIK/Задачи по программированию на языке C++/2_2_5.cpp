c1cc44443db95c9e14f7ed5374b1d55ef938d280
Ekaterina Kozyreva <83861300+E-Kozyreva@users.noreply.github.com>
2022-06-22 23:18:12 +0300

    10, 100, 1000, 10_000
    Напишите программу, которая запрашивает пятиразрядное число и выводит в первой строке -- частное введённого числа и 10, во второй строке -- частное введённого числа и 100, в третьей -- частное при делении на 1000, в четвёртой -- частное при делении на 10000.

---
+++ C++/STEPIK/Задачи по программированию на языке C++/2_2_5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() 
+{
+    int num;
+    std::cin >> num;
+    
+    std::cout << num / 10 << std::endl;
+    std::cout << num / 100 << std::endl;
+    std::cout << num / 1000 << std::endl;
+    std::cout << num / 10000 << std::endl;
+    return 0;
+}
